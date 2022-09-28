3aa7559e1afb5e83a5afd77ee303adeef384db7f
Ferrian <ferrian00@gmail.com>
2022-09-28 18:53:44 +0700

    unmarshall json

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"github.com/ferriantitus/jakmall-backend/models"
+	"io/ioutil"
+	"log"
+)
 
+func main() {
+	product, _ := ioutil.ReadFile("products.json")
+	var dataProduct models.ListProduct
+	err := json.Unmarshal(product, &dataProduct)
+	if err != nil {
+		log.Fatal("Cannot unmarshal the json ", err)
+	}
+
+	review, _ := ioutil.ReadFile("reviews.json")
+	var dataReview models.ListReview
+	err = json.Unmarshal(review, &dataReview)
+	if err != nil {
+		log.Fatal("Cannot unmarshal the json ", err)
+	}
 }
